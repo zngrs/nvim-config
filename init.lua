3a1e44bc7050372afd49cbabfca615db8194181b
zane grosse <zanekg123@gmail.com>
2023-09-05 23:48:32 -0500

    add telescope, coc, barbar, some other minor qol extensions

--- init.lua
+++ init.lua
@@ -1,34 +1,39 @@
 vim.cmd('source ~/.config/nvim/vimscript.vim')
 vim.cmd('source ~/.config/nvim/keybinds.vim')
 
+
 require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'rebelot/kanagawa.nvim'
-	use({
-  "utilyre/barbecue.nvim",
-  tag = "*",
-  requires = {
-    "SmiteshP/nvim-navic",
+    use 'wbthomason/packer.nvim'
+    use 'rebelot/kanagawa.nvim'
+    use({
+        "utilyre/barbecue.nvim",
+        tag = "*",
+        requires = {"SmiteshP/nvim-navic"},
+        after = "nvim-web-devicons", -- keep this if you're using NvChad
+        config = function()
+            require("barbecue").setup()
+        end
+    })
 
-  },
-  after = "nvim-web-devicons", -- keep this if you're using NvChad
-  config = function()
-    require("barbecue").setup()
-  end,
-})
-
-use {
-  "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    requires = { 
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+    use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        requires = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim"}
     }
-  }
+    use {
+  	'glepnir/dashboard-nvim',
+  	event = 'VimEnter',
+  	config = function()
+  	require('dashboard').setup {
+    }
+  end,
+  requires = {'nvim-tree/nvim-web-devicons'}
+}
 end)
 
 vim.cmd("colorscheme nordic")
 vim.cmd('highlight vimwikiHeader1 ctermfg=1')
 vim.cmd('highlight vimwikiHeader2 ctermfg=2')
 vim.cmd('highlight vimwikiHeader3 ctermfg=3')
+
--- vimscript.vim
+++ vimscript.vim
@@ -8,8 +8,15 @@ Plug 'nvim-tree/nvim-tree.lua'
 Plug 'vimwiki/vimwiki'
 Plug 'neoclide/coc.nvim', {'branch': 'release'}
 Plug 'AlexvZyl/nordic.nvim', { 'branch': 'main' }
-
+Plug 'tmsvg/pear-tree'
 Plug 'lervag/vimtex'
+Plug 'lewis6991/gitsigns.nvim' " OPTIONAL: for git status
+Plug 'romgrk/barbar.nvim'
+" Telescope
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.2' }
+
+Plug 'mhinz/vim-startify'
 call plug#end()
 
 let g:airline_theme='minimalist'
@@ -18,3 +25,17 @@ set relativenumber
 set number
 set nocompatible
 filetype plugin on
+
+"Use <tab> for Coc autocomplete
+function! CheckBackspace() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+inoremap <silent><expr> <Tab>
+      \ coc#pum#visible() ? coc#pum#next(1) :
+      \ CheckBackspace() ? "\<Tab>" :
+      \ coc#refresh()
+
+"############
+
