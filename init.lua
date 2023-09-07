6d51d29b5ddb800be7855e76db60bb63d7c16c85
zane grosse <zanekg123@gmail.com>
2023-09-07 13:55:26 -0500

    change vim airline to lua line. Few minor tweaks too

--- init.lua
+++ init.lua
@@ -1,39 +1,82 @@
 vim.cmd('source ~/.config/nvim/vimscript.vim')
 vim.cmd('source ~/.config/nvim/keybinds.vim')
 
-
 require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'rebelot/kanagawa.nvim'
-    use({
-        "utilyre/barbecue.nvim",
-        tag = "*",
-        requires = {"SmiteshP/nvim-navic"},
-        after = "nvim-web-devicons", -- keep this if you're using NvChad
-        config = function()
-            require("barbecue").setup()
-        end
-    })
+	use 'wbthomason/packer.nvim'
+	use 'rebelot/kanagawa.nvim'
+	use({
+  "utilyre/barbecue.nvim",
+  tag = "*",
+  requires = {
+    "SmiteshP/nvim-navic",
 
-    use {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        requires = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim"}
-    }
-    use {
-  	'glepnir/dashboard-nvim',
-  	event = 'VimEnter',
-  	config = function()
-  	require('dashboard').setup {
-    }
+  },
+  after = "nvim-web-devicons", -- keep this if you're using NvChad
+  config = function()
+    require("barbecue").setup()
   end,
-  requires = {'nvim-tree/nvim-web-devicons'}
-}
+})
+
+use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
+  use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
+use 'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
+use 'romgrk/barbar.nvim'
 end)
 
-vim.cmd("colorscheme nordic")
+require('onedark').setup {
+    style = 'darker'
+}
+require('onedark').load()
+
 vim.cmd('highlight vimwikiHeader1 ctermfg=1')
 vim.cmd('highlight vimwikiHeader2 ctermfg=2')
 vim.cmd('highlight vimwikiHeader3 ctermfg=3')
 
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
--- vimscript.vim
+++ vimscript.vim
@@ -1,6 +1,7 @@
 call plug#begin()
-Plug 'vim-airline/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
+"Plug 'vim-airline/vim-airline'
+"Plug 'vim-airline/vim-airline-themes'
+Plug 'nvim-lualine/lualine.nvim'
 
 "File tree + icons
 Plug 'nvim-tree/nvim-web-devicons'
@@ -8,15 +9,13 @@ Plug 'nvim-tree/nvim-tree.lua'
 Plug 'vimwiki/vimwiki'
 Plug 'neoclide/coc.nvim', {'branch': 'release'}
 Plug 'AlexvZyl/nordic.nvim', { 'branch': 'main' }
-Plug 'tmsvg/pear-tree'
-Plug 'lervag/vimtex'
-Plug 'lewis6991/gitsigns.nvim' " OPTIONAL: for git status
-Plug 'romgrk/barbar.nvim'
-" Telescope
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.2' }
+Plug 'navarasu/onedark.nvim'
 
 Plug 'mhinz/vim-startify'
+
+Plug 'lervag/vimtex'
+
+Plug 'lukas-reineke/indent-blankline.nvim'
 call plug#end()
 
 let g:airline_theme='minimalist'
@@ -25,17 +24,3 @@ set relativenumber
 set number
 set nocompatible
 filetype plugin on
-
-"Use <tab> for Coc autocomplete
-function! CheckBackspace() abort
-  let col = col('.') - 1
-  return !col || getline('.')[col - 1]  =~# '\s'
-endfunction
-
-inoremap <silent><expr> <Tab>
-      \ coc#pum#visible() ? coc#pum#next(1) :
-      \ CheckBackspace() ? "\<Tab>" :
-      \ coc#refresh()
-
-"############
-
